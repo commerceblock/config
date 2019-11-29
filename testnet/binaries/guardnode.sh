a65b7a0e9b2454209457a3829e317842a545b0ab
tomossomot <tomoswootton@protonmail.com>
2019-11-29 20:25:15 +0000

    binaries configs

--- testnet/binaries/guardnode.sh
+++ testnet/binaries/guardnode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec $HOME/guardnode/run_guardnode --rpcuser ocean --rpcpass oceanpass --rpchost 127.0.0.1:8332 --servicerpcuser ocean --servicerpcpass oceanpass --servicerpchost 127.0.0.1:8332 --nodelogfile ocean/ocean_main/debug.log --challengehost https://coordinator.testnet.commerceblock.com:10005 --bidlimit 50
+exec $HOME/guardnode/run_guardnode --rpcuser ocean --rpcpass oceanpass --rpchost 127.0.0.1:8332 --servicerpcuser ocean --servicerpcpass oceanpass --servicerpchost 127.0.0.1:8332 --nodelogfile ../ocean/ocean_main/debug.log --challengehost https://coordinator.testnet.commerceblock.com:10005 --bidlimit 50
